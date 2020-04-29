754c7bc0ed74284ee3aede217265c5859f199e87
Haojun Jiang <haojun.jiang@gdit.com>
2020-04-29 01:21:33 -0400

    Add GlitchGarden

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAttacker());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void StopSpawning()
+    {
+        spawn = false;
+    }
+
+    private IEnumerator SpawnAttacker()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            var newAttacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+            newAttacker.transform.parent = this.transform;
+            yield return newAttacker;
+        }
+    }
+}
