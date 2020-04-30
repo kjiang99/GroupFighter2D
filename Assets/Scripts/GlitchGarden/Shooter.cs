42a6d1da8579d7df1b7a5222e78b1a2c8bd7b56d
Haojun Jiang <haojun.jiang@gdit.com>
2020-04-29 23:13:07 -0400

    Add Zucchini

---
+++ Assets/Scripts/GlitchGarden/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+
+    public void Fire()
+    {
+        Instantiate(projectile, this.transform.position, transform.rotation);
+    }
+
+}
