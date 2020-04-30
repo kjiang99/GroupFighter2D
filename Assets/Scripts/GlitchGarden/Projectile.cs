211f04524fd64c22fc279eee25024d32de605c83
Haojun Jiang <haojun.jiang@gdit.com>
2020-04-30 19:05:09 -0400

    Add collision and explosion vfx

--- Assets/Scripts/GlitchGarden/Projectile.cs
+++ Assets/Scripts/GlitchGarden/Projectile.cs
@@ -6,9 +6,23 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 100f;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var attacker = other.GetComponent<Attacker>();
+        var health = other.GetComponent<Health>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(this.gameObject);
+        }
     }
 }
